d2e4afafb54a053b7ef51127de632b31ebecc784
Sivanandan Rajaram <rajaramsiva19@gmail.com>
2025-08-09 07:15:47 +0530

    Time: 0 ms (100%), Space: 42.3 MB (5.35%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,9 +1,13 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-     s=  s.trim();
-     int lastind=s.lastIndexOf(" ");
-     return s.length()-lastind-1;
+        s=s.trim();
+        int count =0;
+        for(int i=s.length()-1;i>=0;i--){
+            char ch=s.charAt(i);
+            if(s.charAt(i)!=' ') count++;
+            else break;
 
-        
+        }
+        return count;
     }
 }
